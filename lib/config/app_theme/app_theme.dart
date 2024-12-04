9b513d4bb256cfa4115cec3fb97381e89264698b
midom <mohsennezar@gmail.com>
2024-12-04 15:58:53 +0200

    +bottom Navbar

--- lib/config/app_theme/app_theme.dart
+++ lib/config/app_theme/app_theme.dart
@@ -3,13 +3,13 @@ import 'package:movie_app/core/colors_manager.dart';
 
 class AppTheme {
   static final ThemeData light = ThemeData(
+    primaryColor: ColorsManager.bottomNavBar,
   scaffoldBackgroundColor: ColorsManager.blackAcc,
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        selectedIconTheme: IconThemeData(size: 30),
         unselectedItemColor: Colors.white,
         selectedItemColor: ColorsManager.selectedItem,
         showUnselectedLabels: true,
-
-
   )
   );
 }
--- lib/presentation/screens/home/home.dart
+++ lib/presentation/screens/home/home.dart
@@ -1,35 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/colors_manager.dart';
+import 'package:movie_app/presentation/screens/home/tabs/home_screen/HomeScreen.dart';
+import 'package:movie_app/presentation/screens/home/tabs/search/SearchTab.dart';
+import 'package:movie_app/presentation/screens/home/tabs/watch_list/WatchList.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+import 'tabs/Browse/Browse.dart';
+
+class Home extends StatefulWidget {
+   Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Widget> tabs = [
+    HomeScreen(),
+    SearchTab(),
+    Browse(),
+    WatchList(),
+  ];
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (value) {
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(canvasColor: Theme.of(context).primaryColor),
+        child: BottomNavigationBar(
 
-        },
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'HOME',
-              backgroundColor: ColorsManager.bottomNavBar),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'SEARCH',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.movie),
-            label: 'BROWSE',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            label: 'WATCHLIST',
-          ),
-        ],
+          onTap: (index) {
+            selectedIndex = index;
+            setState(() {
+            });
+          },
+          currentIndex: selectedIndex,
+
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'HOME',
+
+               ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'SEARCH',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.movie),
+              label: 'BROWSE',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'WATCHLIST',
+            ),
+          ],
+        ),
       ),
+      body: tabs[selectedIndex],
+
+
     );
   }
 }
