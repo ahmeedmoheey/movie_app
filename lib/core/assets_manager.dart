7590733e6aba952dd65b99cb9ebfb71055bcd021
midom <mohsennezar@gmail.com>
2024-12-04 21:01:12 +0200

    +Search screen ui

--- lib/core/assets_manager.dart
+++ lib/core/assets_manager.dart
@@ -1,3 +1,4 @@
 class AssetsManager {
-  static const String movieLogo= 'assets/images/movies.png';
+  static const String movieLogo = 'assets/images/movies.png';
+  static const String searchNoMoviesFound = 'assets/images/no_movies_found.png';
 }
--- lib/core/colors_manager.dart
+++ lib/core/colors_manager.dart
@@ -4,4 +4,5 @@ class ColorsManager{
   static const Color blackAcc = Color(0xFF121312);
   static const Color bottomNavBar = Color(0xFF1A1A1A);
   static const Color selectedItem = Color(0xFFFFA90A);
+  static const Color searchBar = Color(0xFF514F4F);
 }
