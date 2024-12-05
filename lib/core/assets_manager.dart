2745da30f80d6dc120ecb1cec93be20ed5fc45fb
ahmed <ahmedmohie553@gmail.com>
2024-12-05 17:02:34 +0200

    +app_styles +browser +browserCategory +constantManager +stringsManager +categoryDm

--- lib/core/assets_manager.dart
+++ lib/core/assets_manager.dart
@@ -1,4 +1,23 @@
 class AssetsManager {
   static const String movieLogo = 'assets/images/movies.png';
   static const String searchNoMoviesFound = 'assets/images/no_movies_found.png';
+  static const String actionFilm = 'assets/images/action.png';
+  static const String adventureFilm = 'assets/images/adventure.png';
+  static const String animationFilm = 'assets/images/animation.png';
+  static const String comedyFilm = 'assets/images/comedy.png';
+  static const String crimeFilm = 'assets/images/crime.png';
+  static const String documenteryFilm = 'assets/images/documentery.png';
+  static const String dramaFilm = 'assets/images/drama.png';
+  static const String familyFilm = 'assets/images/family.png';
+  static const String fantasyFilm = 'assets/images/fantasy.png';
+  static const String historyFilm = 'assets/images/history.png';
+  static const String horroFilm = 'assets/images/horro.png';
+  static const String musicFilm = 'assets/images/music.png';
+  static const String mysteryFilm = 'assets/images/mystery.png';
+  static const String romanceFilm = 'assets/images/romance.png';
+  static const String scienceFilm = 'assets/images/science.png';
+  static const String thrillerFilm = 'assets/images/thriller.png';
+  static const String tvMovieFilm = 'assets/images/tv_movie.png';
+  static const String warFilm = 'assets/images/war.png';
+  static const String westernFilm = 'assets/images/western.png';
 }
