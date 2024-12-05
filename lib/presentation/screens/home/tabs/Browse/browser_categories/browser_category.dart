2186f9d5ca285c73bfeedefbaa94038fbf0db352
midom <mohsennezar@gmail.com>
2024-12-05 20:02:30 +0200

    +browse Ui

--- lib/presentation/screens/home/tabs/Browse/browser_categories/browser_category.dart
+++ lib/presentation/screens/home/tabs/Browse/browser_categories/browser_category.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movie_app/config/app_styles/app_styles.dart';
 import 'package:movie_app/data_model/category_dm/category_dm.dart';
+import 'package:movie_app/my_application/my_application.dart';
 
 class BrowserCategories extends StatelessWidget {
    BrowserCategories({super.key, required this.categoryDM, required this.index});
@@ -14,8 +16,9 @@ class BrowserCategories extends StatelessWidget {
       children: [
         Image.asset(
          categoryDM.imagePath,
-          width: 158,
-          height: 90,
+          width: 158.w,
+          height: 90.h,
+          fit: BoxFit.cover,
         ),
         Text(categoryDM.title,
         style: AppStyles.browserCategories,)
